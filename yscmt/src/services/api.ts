e6a099960ff3bdf114d2653fc2115b1a2870cc5f
hackeryoung34-netizen <hackeryoung34@gmail.com>
2026-08-02 16:34:53 +0100

    Create migrations

--- yscmt/src/services/api.ts
+++ yscmt/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://yscmtcommunty-production.up.railway.app/api/",
+baseURL: "https://yscmtcommunty-production.up.railway.app/api/",
 });
 
 api.interceptors.request.use((config) => {
